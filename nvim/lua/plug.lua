980a0c75f178f29cf23df0c4672b0dcc6f294adf
Thalles Rodrigues <ts.rodr@gmail.com>
2023-01-28 20:01:20 -0300

    minor edit to plug

--- nvim/lua/plug.lua
+++ nvim/lua/plug.lua
@@ -2,18 +2,20 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
--- Plug 'itchyny/lightline.vim'                           -- status bar
--- Plug 'shinchu/lightline-gruvbox.vim'                   -- lightline theme (req: lightline)
--- Plug 'morhetz/gruvbox'                              -- theme
+-- Theme & Status Bar
 Plug 'vim-airline/vim-airline'
 Plug 'vim-airline/vim-airline-themes'
-Plug 'airblade/vim-gitgutter'                          -- displays git diff column
 Plug 'ellisonleao/gruvbox.nvim' 
+
+-- Other plugins
+Plug 'airblade/vim-gitgutter'                 -- displays git diff column
+Plug 'ellisonleao/glow.nvim'                  -- TUI markdown previewer
 Plug 'nvim-lua/popup.nvim'
 Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-telescope/telescope.nvim'                   -- finder (req: popup, plenary)
+Plug 'nvim-telescope/telescope.nvim'          -- finder (req: popup, plenary)
+
+-- Language-related stuff
 Plug 'neovim/nvim-lspconfig'
-Plug 'ellisonleao/glow.nvim'
 Plug 'othree/html5.vim'
 Plug 'pangloss/vim-javascript'
 Plug('evanleck/vim-svelte', {branch = 'main'})
